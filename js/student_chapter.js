2caa9dc19f330626358371000dca67ec1588f83d
ikadelharder <S02194169@FRCCinst.ccofc.edu>
2017-04-18 21:04:38 -0600

    End of night - working map
    Worked with David to get map showing up on webpage from Brackets

--- js/student_chapter.js
+++ js/student_chapter.js
@@ -1,6 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+
+  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    var studentChptBase = L.tileLayer('https://api.mapbox.com/styles/v1/ikadelharder/cj1ocv3gi000a2spfgl8dii3m/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWthZGVsaGFyZGVyIiwiYSI6ImNpeTJjazZueTAwMWkyemxhaXkzamI4emwifQ.txLrrcdZbc6hF4UfpKNClA', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18
+    }).addTo(mymap);
+
+
     /*
     your code will go here
     */
-    
+
 });

967a86a3e9d71750b6b7bb47a3fc86e553f0f50c
ikadelharder <S02194169@FRCCinst.ccofc.edu>
2017-04-25 19:44:31 -0600

    no points on mapbox map
    removed and replaced with basic map, something wrong with fetch though

--- js/student_chapter.js
+++ js/student_chapter.js
@@ -1,14 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  var mymap = L.map('mapid').setView([38.0000, -97.0000], 5);
-    var studentChptBase = L.tileLayer('https://api.mapbox.com/styles/v1/imkh/cj1uwhgey00032rqh52dk93hy/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaW1raCIsImEiOiJjaXpxY3hldzAwMHc3MnFueWlxdHM3dm1uIn0.gAsO-CCt32aUQ9lRuDoYwA', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 18
-    }).addTo(mymap);
+            var mymap = L.map('mapid').setView([38.0000, -97.0000], 5);
+            var lightBase = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaW1raCIsImEiOiJjaXpxY3hldzAwMHc3MnFueWlxdHM3dm1uIn0.gAsO-CCt32aUQ9lRuDoYwA', {
+                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                maxZoom: 18
+            }).addTo(mymap);
 
-
-    /*
-    your code will go here
-    */
+fetch('/features.geojson')
+.then(function(response)
+ {
+ return
+ response.json();
+})
+.then(function(geojson)
+ {
+ L.geoJSON(geojson,
+   ).addTo(mymap);
+});
+    
+    
+//var baseGeol = L.tileLayer.wms("https://mrdata.usgs.gov/wfs/kb?request=getfeature&service=WFS&version=1.1.0&typename=Geology,Faults&maxfeatures=10", {
+    //layers: 'nexrad-n0r-900913',
+    //format: 'image/png',
+    //transparent: true,
+    //attribution: "Weather data © 2012 IEM Nexrad"
+    
 
 });
